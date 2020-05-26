272864c452aefd5de27b69351bf4045adf3d633d
shyam9741405520 <64199515+shyam9741405520@users.noreply.github.com>
2020-05-26 10:54:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+ background(255,0,0);  
+ rectMode(CENTER)
+ ellipseMode(RADIUS)
+ rect(200,350,20,100);
+ fill("purple");
+ ellipse(200,32030,30);
+ fill("blue");
+ rect(100,300,30,200);
+ fill("green");
+ rect(300,300,30,200);
+ fill("green");
+ triangle(85,200,115,200,100,170);
+ fill("yellow");
+ triangle(285,200,315,200,300,170);
+ fill("yellow");
+ rect(200,390,180,20);
+ fill("black");
+ rect(200,270,180,20);
+ fill("pink");
+ drawSprites();
+}
+  
+
+
+
